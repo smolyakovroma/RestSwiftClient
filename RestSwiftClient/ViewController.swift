58c23fe793db76273460309b57807a9811e2a07b
Роман Смоляков <romansmolakov@192.168.0.101.ipl.orionnet.ru>
2018-08-04 13:33:10 +0700

    Initial Commit

---
+++ RestSwiftClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RestSwiftClient
+//
+//  Created by Роман Смоляков on 04.08.2018.
+//  Copyright © 2018 Роман Смоляков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
